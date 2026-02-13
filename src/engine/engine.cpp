e3f50b9ec05dd083ba7d56955b01efd80579a334
Vraj Patel <108849348+P-Vraj@users.noreply.github.com>
2026-02-13 00:08:03 -0500

    Update engine testing

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include "types.h"
 #include "util.h"
-#include "move_gen_tables.h"
+#include "move_gen/attacks.h"
 
 int main() {
-    // constexpr auto knightAttacks = getKnightAttacks();
-    std::cout << to_string(kKnightAttacks[0]) << '\n';
+    init_attack_tables();
 
-    // constexpr auto kingAttacks = getKingAttacks();
-    std::cout << to_string(kKingAttacks[0]) << '\n';
-
-    // constexpr auto whitePawnAttacks = getPawnAttacks<Color::White>();
-    std::cout << to_string(kPawnAttacks[to_underlying(Color::White)][0]) << '\n';
-
-    // constexpr auto blackPawnAttacks = getPawnAttacks<Color::Black>();
-    std::cout << to_string(kPawnAttacks[to_underlying(Color::Black)][0]) << '\n';
+    Bitboard blockers = bitboard(Square::B2) | bitboard(Square::F1);
+    std::cout << "Blockers:\n" << to_string(blockers) << '\n';
+    std::cout << "King attacks:\n" << to_string(king_attacks(Square::D4)) << '\n';
+    std::cout << "White pawn attacks:\n" << to_string(pawn_attacks(Color::White, Square::D4)) << '\n';
+    std::cout << "Black pawn attacks:\n" << to_string(pawn_attacks(Color::Black, Square::D4)) << '\n';
+    std::cout << "Knight attacks:\n" << to_string(knight_attacks(Square::D4)) << '\n';
+    std::cout << "Bishop attacks:\n" << to_string(bishop_attacks(Square::C1, blockers)) << '\n';
+    std::cout << "Rook attacks:\n" << to_string(rook_attacks(Square::A1, blockers)) << '\n';
+    std::cout << "Queen attacks:\n" << to_string(queen_attacks(Square::C1, blockers)) << '\n';
 
     return 0;
 }
